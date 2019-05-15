e2a457d04bf43c0dbff4d09bd94fa01fd59291b1
Jessica Wailes <jess.m.wailes@gmail.com>
2019-05-15 14:49:09 -0700

    first commit of new project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# TotesOriginal" 
---
+++ XamarinFrameworkApp/XamarinFrameworkApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFrameworkApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e) /*this is the method that is called when 'Botton_Clicked' on the .xaml file is referenced*/
+        {
+
+        }
+    }
+}
